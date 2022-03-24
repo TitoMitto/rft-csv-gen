9ab154d3388e02bbf6227cd4e04444ee4766379f
Tito Mitto <mordeccai9@gmail.com>
2022-03-24 19:30:28 +0300

    generating csv

--- main.ts
+++ main.ts
@@ -1,34 +1,45 @@
 import Denomander from "https://deno.land/x/denomander/mod.ts";
 
 const program = new Denomander({
-  app_name: "RTF Reader",
+  app_name: "RTF to CSV Generator",
   app_description:
-    "This app will help you generate FS Number of printed orders",
+    "This program will help you generate csv of FSNumbers of printed sales",
   app_version: "1.0.0",
 });
 
 program
   .command("generate")
-  .requiredOption("-i --input", "Define the input directory")
-  .requiredOption("-o --output", "Define the output path")
+  .requiredOption("-i --input", "Define the input file or directory")
+  .option("-o --output", "Define the output path")
   .parse(Deno.args);
+
 
 if (program.input) {
   let data = "ORDER ID, FS NUMBER, CUSTOMER, DATE\n";
 
-  for await (const dirEntry of Deno.readDir(program.input)) {
-    if (dirEntry.isFile && dirEntry.name.endsWith(".rtf")) {
-      const fileContents = await Deno.readTextFile(dirEntry.name);
+  if(program.input.endsWith(".rtf")){
+    const fileContents = await Deno.readTextFile(`${program.input}`);
+    data = generateText(fileContents, data);
+  } else{ 
+    for await (const dirEntry of Deno.readDir(program.input)) {
+        if (dirEntry.isFile && dirEntry.name.endsWith(".rtf")) {
+          const fileContents = await Deno.readTextFile(`${program.input}/${dirEntry.name}`);
+          data =generateText(fileContents, data)
+        }
+      }
+  }
+  
+  await Deno.writeTextFile(`${program.output ?? "output"}.csv`, data);
+}
 
-      const matches = fileContents.matchAll(
+
+function generateText(fileContents: string, data:string){
+    const matches = fileContents.matchAll(
         /FS\sNo.(.+?)\s*DATE:\s(.*)\n*.*\n*\n*(Customers\sTIN.*)?\n*Customer:\s(.*)\nRef:\s(.*)/g,
       );
 
       for (const x of matches) {
         data += `${x[5]}, ${x[1]}, ${x[4]}, ${x[2]}\n`;
       }
-    }
-  }
-
-  await Deno.writeTextFile(`${program.output}.csv`, data);
+      return data;
 }
